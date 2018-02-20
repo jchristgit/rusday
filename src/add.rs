963ed625394db764148a1f1e1168f8d69646665b
Johannes Christ <Johannes.christ@gmail.com>
2018-02-20 15:13:49 +0100

    Move table setup to `common`, add date error handling.

--- src/add.rs
+++ src/add.rs
@@ -15,16 +15,15 @@ pub struct Person {
 
 pub fn add_entry(date: &str, name: &str) {
     let conn = get_db_conn();
-    conn.execute("CREATE TABLE IF NOT EXISTS person (
-        id      INTEGER PRIMARY KEY,
-        date    DATE NOT NULL,
-        name    TEXT NOT NULL
-        )", &[]).unwrap();
-    let new_entry = Person {
-        id: 0,
-        date: NaiveDate::parse_from_str(date, "%d-%m-%Y").unwrap(),
-        name: String::from(name)
-    };
-    conn.execute("INSERT INTO person (date, name) VALUES (?1, ?2)", &[&new_entry.date, &new_entry.name]).unwrap();
+    if let Ok(date) = NaiveDate::parse_from_str(date, "%d-%m-%Y") {
+        let new_entry = Person {
+            id: 0,
+            date: date,
+            name: String::from(name)
+        };
+        conn.execute("INSERT INTO person (date, name) VALUES (?1, ?2)", &[&new_entry.date, &new_entry.name]).unwrap();
+    } else {
+        eprintln!("Failed to parse a date from {}. Are you sure it's formatted correctly?", date);
+    }
     let _ = conn.close();
 }
--- src/common.rs
+++ src/common.rs
@@ -8,9 +8,14 @@ pub fn get_db_conn() -> rusqlite::Connection {
     let mut db_path_buf = env::home_dir().unwrap();
     db_path_buf.push(".local/share/rusday.db");
     if let Ok(conn) = Connection::open(db_path_buf.as_path()) {
+        conn.execute("CREATE TABLE IF NOT EXISTS person (
+            id      INTEGER PRIMARY KEY,
+            date    DATE NOT NULL,
+            name    TEXT NOT NULL
+        )", &[]).unwrap();
         conn
     } else {
         eprintln!("Failed to establish a database connection...");
-        process::exit(-1);
+        process::exit(1);
     }
 }
